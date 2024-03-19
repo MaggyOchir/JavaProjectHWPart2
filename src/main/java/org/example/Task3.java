5066398ed363eec269322e66a8715d5ab7f22a71
MaggyOchir <munhmandah@gmail.com>
2024-03-19 11:15:55 -0700

    HW1-5 is done

--- src/main/java/org/example/Task3.java
+++ src/main/java/org/example/Task3.java
@@ -14,14 +14,8 @@ public class Task3 {
                     }
                 }
             }
-        System.out.println();
-        System.out.print("odd numbers: ");
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = 0; j < numbers[i].length; j++) {
-                if (numbers[i][j] % 2 != 0) {
-                    System.out.print(numbers[i][j] + " ");
-                }
-            }}}}
+
+            }}
 
 
 
--- src/main/java/org/example/Task5.java
+++ src/main/java/org/example/Task5.java
@@ -3,6 +3,13 @@ package org.example;
 //Write a program to swap 2 numbers without a temporary variable?
 public class Task5 {
     public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        System.out.println("original numbers "+a+" "+b);
+        a=b;
+        b=a;
+        System.out.println("after swapping "+a+" "+b);
+
 
                     }
                 }
