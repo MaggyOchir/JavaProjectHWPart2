67986d958f0bf3fa6ac5864e18306ddadcbc0f2f
MaggyOchir <munhmandah@gmail.com>
2024-03-19 11:32:27 -0700

    HW1-5 is done

---
+++ src/main/java/org/example/Task6.java
@@ -0,0 +1,23 @@
+package org.example;
+
+//6.	Write a java program to check whether a given number is prime or not?
+public class Task6 {
+    public static void main(String[] args) {
+        int[]numbers={6,7,8,9,10,11,12};
+
+
+
+                    }
+                }
+
+
+
+
+
+
+
+
+
+
+
+
